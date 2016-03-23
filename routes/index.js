d5f01d800e42809373a1f85483ef6f37c8f55e8c
Alysson Nazareth <Alysson@iMac.local>
2016-03-23 19:16:00 -0300

    ...

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ var path = require('path');
 
 /* GET partials for angular routeProvider pages. */
 router.get('/views/:folder/partials/:file', function (req, res, next) {
-    console.log(__dirname);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    
     res.set('Content-Type', 'text/html').sendFile(path.join(__dirname + './../../views/'+req.params.folder +'/partials/'+ req.params.file+'.html'));
 });
 
