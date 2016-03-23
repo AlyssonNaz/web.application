d6872d49802a9fc5db736f07f16bd188f61f5947
Alysson Nazareth <Alysson@iMac.local>
2016-03-23 19:28:58 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var db = require('rah.db');
+var utils = require('rah.utils');
 var path = require('path');
 
 /* GET partials for angular routeProvider pages. */
 router.get('/views/:folder/partials/:file', function (req, res, next) {
     console.log(path.join(__dirname + './../../views/'+req.params.folder +'/partials/'+ req.params.file+'.html'));
+    
+    utils.io.forEachDir('/../../views/', true, function (file, filePath) {
+            console.log(filePath);
+        });
     res.set('Content-Type', 'text/html').sendFile(path.join(__dirname + './../../views/'+req.params.folder +'/partials/'+ req.params.file+'.html'));
 });
 
