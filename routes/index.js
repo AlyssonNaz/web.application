f63d5534aef08db9458647249f5438d04ea2aa97
Alysson Nazareth <Alysson@iMac.local>
2016-03-23 19:36:02 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require('rah.db');
 var utils = require('rah.utils');
 var path = require('path');
+var fs = require('fs');
 
 /* GET partials for angular routeProvider pages. */
 router.get('/views/:folder/partials/:file', function(req, res, next) {
