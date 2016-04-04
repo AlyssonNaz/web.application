63e2a41505c49df7ddf887bb8e0e06ec3839bc70
Alysson Nazareth <Alysson@iMac.local>
2016-04-04 00:42:46 -0300

    criptografia de requisição

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,39 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var db = require('rah.db');
+var crypto = require('rah.utils').crypto;
 var TUser = require('rah.modules')('user');
 
 //gera um token de autenticação para um usuário através do nome de usuário ou e-mail, e a senha
-router.post('/', function (req, res, next) {
-    
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+    req.body = crypto.decrypt(req.body);
+    console.log(req.body);
+
     if (!req.body.username)
         return res.status(400).json({ error: 'Usuário não informado.', body: req.body });
-    
+
     if (!req.body.password)
         return res.status(400).json({ error: 'Senha não informada.' });
-        
+
     TUser.authenticate(
         {
             username: req.body.username,
             password: req.body.password
         }
-        ).then(function (token) {
-            return res.json({ token });
-        }, function (err) {
-            return res.status(401).json(err);
-        });
+    ).then(function(token) {
+        return res.json({ token });
+    }, function(err) {
+        return res.status(401).json(err);
+    });
 });
 
 //cria um novo usuário com token
-router.post('/register', function (req, res, next) {
+router.post('/register', function(req, res, next) {
     TUser.create(
         {
             username: req.body.username,
             email: req.body.email,
             password: req.body.password
-        }).then(function (token) {
+        }).then(function(token) {
             return res.json({ token });
-        }, function (err) {
+        }, function(err) {
             return res.status(401).json(err);
         });
 });
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ var fs = require('fs');
 
 /* GET partials for angular routeProvider pages. */
 router.get('/views/:folder/partials/:file', function(req, res, next) {   
-    console.log(process.env.ROOT_DIR);
     res.set('Content-Type', 'text/html').sendFile('/views/' + req.params.folder + '/partials/' + req.params.file + '.html',  { root: process.env.ROOT_DIR });
 });
 
