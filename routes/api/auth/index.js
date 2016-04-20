447769a54188942846d4cbf8bba0c4274fcaa6a4
Alysson Nazareth <Alysson@iMac.local>
2016-04-19 22:53:31 -0300

    implementado middleware para dividir requisições web x mobille

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -1,45 +1,60 @@
 var express = require('express');
 var router = express.Router();
 var db = require('rah.db');
-var crypto = require('rah.utils').crypto;
+var auth = require('rah.auth');
 var TUser = require('rah.modules')('user');
 
+//MARK: Routes
+
 //gera um token de autenticação para um usuário através do nome de usuário ou e-mail, e a senha
-router.post('/', function(req, res, next) {
-    console.log(req.body);
-    var data = crypto.decrypt(req.body.data);    
-    console.log(data);
+router.post('/', function (req, res, next) {    
+    return Login(body.username, body.password);
+});
 
-    if (!data.username)
-        return res.status(400).json({ error: 'Usuário não informado.', body: req.body });
-
-    if (!data.password)
-        return res.status(400).json({ error: 'Senha não informada.' });
-        
-    TUser.authenticate(
-        {
-            username: data.username,
-            password: data.password
-        }
-    ).then(function(token) {
-        return res.json({ token });
-    }, function(err) {
-        return res.status(401).json(err);
-    });
+router.post('/mob', auth.crypto, function (req, res, next) {
+    return Login(data.username, data.password);
 });
 
 //cria um novo usuário com token
-router.post('/register', function(req, res, next) {
+router.post('/register', function (req, res, next) {
+    return Register(body.email, body.username, body.password);
+});
+
+router.post('/mob/register', auth.crypto, function (req, res, next) {    
+    return Register(data.email, data.username, data.password);
+});
+
+//MARK: Methods
+function Login(username, password) {
+    if (!username)
+        return res.status(400).json({ error: 'Usuário não informado.'});
+
+    if (!password)
+        return res.status(400).json({ error: 'Senha não informada.' });
+
+    TUser.authenticate(
+        {
+            username: username,
+            password: password
+        }
+    ).then(function (token) {
+        return res.json({ token });
+    }, function (err) {
+        return res.status(401).json(err);
+    });
+}
+
+function Register(email, username, password) {
     TUser.create(
         {
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
-        }).then(function(token) {
+            username: username,
+            email: email,
+            password: password
+        }).then(function (token) {
             return res.json({ token });
-        }, function(err) {
+        }, function (err) {
             return res.status(401).json(err);
         });
-});
+}
 
 module.exports = router;
